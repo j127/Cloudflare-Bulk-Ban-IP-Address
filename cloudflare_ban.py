07856075cd0c0da5cb9b0a163323b4144f51f6b4
J. Cohen <hostelmarketing@gmail.com>
2015-04-01 00:26:02 -0700

    Working version

--- cloudflare_ban.py
+++ cloudflare_ban.py
@@ -11,6 +11,7 @@ api_email_address = cf_settings.api_email_address
 
 # Configure logfile -- comment this out if you don't want a logfile
 logging.basicConfig(filename="cloudflare_bans.log", format="%(asctime)s\t%(levelname)s:\t%(message)s", level=logging.INFO)
+# TODO: implement logging so that errors can be reviewed later
 
 def post_to_cloudflare(api_token, api_email_address, action, ip_address):                     
     """Post the the Cloudflare API.
@@ -32,19 +33,16 @@ def post_to_cloudflare(api_token, api_email_address, action, ip_address):
 
     r = requests.post("https://www.cloudflare.com/api_json.html", data=payload)
     response = r.text
-    response_dict = json.loads(response)
+    response_dict = json.loads(response) # TODO: conditionally log error messages
     print("Printing response: ", response)
 
-    if response_dict["result"] != "success":
-        logging.warning("Something went wrong with the response for ", response_dict["ip"])
-    else:
-        logging.info("Successfully banned IP", response_dict["ip"])
-
 def read_file_and_ban():
+    """Reads IP addresses from a file and bans them."""
+    
     f = open('banned_ips.txt')
     for line in f:
         cleaned_line = line.strip()
-        print("About to ban: ", cleaned_line)
+        print("About to ban:", cleaned_line)
         post_to_cloudflare(api_token, api_email_address, "ban", cleaned_line)
         time.sleep(0.5) # ensure that it stays under 1,200 requests in five minutes. This limits it to 600.
     f.close()
