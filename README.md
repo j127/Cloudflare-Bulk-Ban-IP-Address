6cfc4efa979efd5e8c5aecf731dee055c9a2854a
J. Cohen <hostelmarketing@gmail.com>
2015-03-31 22:51:14 -0700

    Rearrange files

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Cloudflare IP Address Banning
 
-This simple Python 3 script bans IP addresses in bulk. It was written on Ubuntu 14.10, but should run on other systems.
+*WARNING:* Do not use this script unless you know exactly what it does!
 
-It reads from a file that contains a list of IP addresses, one per line. It logs to cloudflare_ban_log.txt.
+This simple Python 3 script bans spammer IP addresses in bulk. It was written on Ubuntu 14.10, but should run on other systems.
+
+It reads from a text file that contains a list of IP addresses, one per line. It logs results to a text file.
+
+For now, this is just designed to bulk-ban IP addresses on Cloudflare. Maybe later it will have more functionality.
+
--- cf_ip_ban/cloudflare_ban.py
+++ cf_ip_ban/cloudflare_ban.py
@@ -1,19 +1,24 @@
 import os
+import json
 import logging
 import requests
 
-api_token_variable_name = "CF_API_TOKEN"
-api_email_address_variable_name = "CF_EMAIL_ADDRESS"
+# Allow changing of environment variable names
+CF_API_TOKEN = "CF_API_TOKEN"
+CF_EMAIL_ADDRESS = "CF_EMAIL_ADDRESS"
+
+# Configure logfile
+logging.basicConfig(filename="cloudflare_bans.log", format="%(asctime)s\t%(levelname)s:\t%(message)s", level=logging.INFO)
 
 # Move to end
-save_status = input("Do you want to save API credentials as environment variables?")
+save_status = input("Do you want to save your API credentials as environment variables? [y/N]")
 save_api_tokens(save_status)
 
 def get_api_credentials():
     """Check if API credentials are saved in environment variables."""
 
-    api_token = os.environ[api_token_variable_name]
-    api_email_address = os.environ[api_email_address_variable_name]
+    api_token = os.environ[CF_API_TOKEN]
+    api_email_address = os.environ[CF_EMAIL_ADDRESS]
 
 def save_api_tokens(save_status):
     """A way to save API credentials as environment variables on servers."""
@@ -24,6 +29,8 @@ def save_api_tokens(save_status):
 
 def post_to_cloudflare(api_token, api_email_address, action, ip_address):                     
     """Post the the Cloudflare API.
+    
+    Don't post more than 1,200 times in five minutes.
 
     tkn -- API token
     email -- the email address associated with the Cloudflare account
@@ -39,10 +46,14 @@ def post_to_cloudflare(api_token, api_email_address, action, ip_address):
     }
 
     r = requests.post("https://www.cloudflare.com/api_json.html", data=payload)
-    print(r.text)
+    response = r.text
+    response_dict = json.loads(response)
+    if response_dict["result"] not "success":
+        logging.error("Something went wrong with the response for ", key)
+
 
 def read_file():
     f = open('banned_ips.txt')
     for line in f:
-        print f
+        print("About to ban: ", f)
 
